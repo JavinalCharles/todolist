6f53f5ff8d2b20ed767d7ccb2443d7bedbabeb35
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-08-20 15:22:01 +0800

    Scratched most of home.ejs and opted for dynamically appending to-do boxes in the #main_section. Should allow for sorting functionality in client web page later on.

--- public/JS/home.js
+++ public/JS/home.js
@@ -14,7 +14,6 @@ function sendDoneTaskRequest(event) {
 		if (httpRequest.readyState === XMLHttpRequest.DONE) {
 			// Everything is good, the response was received.
 			if (httpRequest.status == 200) {
-				t.removeAttribute("disabled");
 				console.log("DONE!");
 				console.log(httpRequest.responseText);
 
@@ -27,11 +26,9 @@ function sendDoneTaskRequest(event) {
 				parentDiv.insertAdjacentElement("beforeEnd", wholeTodoDiv);
 			}
 			else {
+				t.removeAttribute("disabled");
 				console.log(httpRequest.responseText);
 			}
-		} else {
-			// Not ready yet.
-			console.log("NOT YET DONE!");
 		}
 	}
 
@@ -42,24 +39,38 @@ function sendDoneTaskRequest(event) {
 function submitAddTask() {
 	addTaskButton.setAttribute("disabled", "");
 	let p = document.getElementById("priority-selection");
+	let newTask = taskInput.value;
+	let pID = p.value;
+	let pText = p.options[p.selectedIndex].text;
 
 	const httpRequest = new XMLHttpRequest();
 
 	httpRequest.onreadystatechange = () => {
 		if (httpRequest.readyState == XMLHttpRequest.DONE) {
 			addTaskButton.removeAttribute("disabled");
+			let response = JSON.parse(httpRequest.responseText);
+			console.log(response);
+			let taskID = response.insertId;
 			if (httpRequest.status == 200) {
-				taskInput.value = "";
+				let newToDo = {
+					todoID: taskID,
+					tasks: newTask,
+					priorityID: pID,
+					text: pText
+				};
+				todolist.push(newToDo);
+				createToDoBox(newToDo);
 
+				taskInput.value = "";
 				let options = p.options;
 				for (let i = 0; i < options.length; ++i) {
 					options[i].selected = false;
 				}
 			}
-			// console.log(httpRequest.responseText);
-		}
-		else {
-			console.log(httpRequest.responseText);
+			else {
+				addTaskButton.removeAttribute("disabled");
+				console.log(httpRequest.responseText);
+			}
 		}
 	}
 	httpRequest.open("POST", "http://localhost:3000/", true);
@@ -76,6 +87,84 @@ function submitAddTask() {
 	httpRequest.send(data);
 }
 
-doneButtons.forEach((buttonElement) => {
-	buttonElement.addEventListener("click", sendDoneTaskRequest);
-});
+let main = document.getElementById("main_section");
+
+let createToDoBox = (todoObj) => {
+	let taskID = todoObj.todoID;
+	let taskName = todoObj.tasks;
+	let priorityID = todoObj.priorityID;
+	let priorityText = todoObj.text;
+
+	let newBox = document.createElement("div");
+	newBox.classList.add("grid-box", "todo-item-container");
+
+	let idBox = document.createElement("div");
+	idBox.classList.add("grid-item", "gi-xss-2", "gi-xs-6", "gi-sm-6");
+	let idP = document.createElement("p");
+	idP.classList.add("task-id-p");
+	idP.innerHTML = "#" + taskID;
+	idBox.appendChild(idP);
+	newBox.appendChild(idBox);
+
+	let priorityBox = document.createElement("div");
+	priorityBox.classList.add("grid-item", "gi-xxs-10", "gi-xs-6", "gi-sm-6", "priority-div");
+	let priorityP = document.createElement("p");
+	priorityP.classList.add("priority-p");
+	priorityP.innerHTML = "priority level: " + priorityText;
+	priorityBox.appendChild(priorityP);
+	newBox.appendChild(priorityBox);
+
+	let taskBox = document.createElement("div");
+	taskBox.classList.add("grid-item", "gi-xxs-12", "gi-xs-12", "gi-sm-6");
+	let taskP = document.createElement("p");
+	taskP.classList.add("tasks-p");
+	taskP.innerHTML = taskName;
+	taskBox.appendChild(taskP);
+	newBox.appendChild(taskBox);
+
+	let actionsBox = document.createElement("div");
+	actionsBox.classList.add("grid-item", "gi-xxs-12", "gi-xs-12", "gi-sm-6", "action-div");
+	let actionsFlex = document.createElement("div");
+	actionsFlex.classList.add("flex-buttons", "flex-box", "flex-xss-jc", "flex-sm-je");
+	if (priorityID > 1) {
+		let doneButton = document.createElement("button");
+		doneButton.classList.add("btn", "done-btn");
+		doneButton.setAttribute("type", "button");
+		doneButton.setAttribute("value", taskID);
+		doneButton.innerHTML = "Done";
+		doneButton.addEventListener("click", sendDoneTaskRequest);
+		actionsFlex.appendChild(doneButton);
+	}
+
+	let delButton = document.createElement("button");
+	delButton.classList.add("btn", "del-btn");
+	delButton.setAttribute("type", "button");
+	delButton.setAttribute("value", taskID);
+	delButton.innerHTML = "Delete";
+	delButton.addEventListener("click", function(e){
+		console.log("Delete Button clicked for task ID: " + e.target.value)
+	});
+
+	actionsFlex.appendChild(delButton);
+	actionsBox.appendChild(actionsFlex);
+	newBox.appendChild(actionsBox);
+
+	// Append created elements
+	main.appendChild(newBox);
+
+	setTimeout(() => {
+		newBox.classList.add("left-transition");
+	}, 10);
+}
+
+if (todolist && main) {
+	console.log("Rendering Tasks");
+	for(let i = 0; i < todolist.length; ++i) {
+		createToDoBox(todolist[i]);
+	}
+	console.log("Rendering finished.");
+}
+
+// doneButtons.forEach((buttonElement) => {
+// 	buttonElement.addEventListener("click", sendDoneTaskRequest);
+// });
